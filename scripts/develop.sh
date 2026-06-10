2ffbf783b7448495d7f793e4e0ace77797dd16ae
Bert Proesmans <bert.proesmans@e-powerinternational.com>
2026-06-10 10:04:22 +0000

    Add scripts to support the writing process

---
+++ scripts/develop.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env nix
+#! nix shell nixpkgs#nodejs_22 --command bash
+
+# npm run clean
+# npm run clean:og
+npm run dev:11ty
---
+++ scripts/install.sh
@@ -0,0 +1,4 @@
+#! /usr/bin/env nix
+#! nix shell nixpkgs#nodejs_22 --command bash
+
+npm ci
